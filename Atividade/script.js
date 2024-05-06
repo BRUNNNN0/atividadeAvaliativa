d4a2902038675cca6feb7f7911d78c78aa608362
BRUNNNN0 <169107024+BRUNNNN0@users.noreply.github.com>
2024-05-06 20:44:29 -0300

    Add files via upload

---
+++ Atividade/script.js
@@ -0,0 +1,32 @@
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let nomeRegex = /^[^0-9]{1,100}$/;
+let numeroRegex = /^\d{2}9?\d{8,9}$/;
+function SalvarDados() {
+
+
+}
+
+function validarCampos() {
+  let nome = document.getElementById('nome');
+  let email = document.getElementById('email');
+  let numero = document.getElementById('numero');
+
+  if (nome.value == "" || email.value == "" || numero.value == "") {
+    alert("Preencha todos os campos");
+  } else if (!emailRegex.test(email.value)) {
+    alert("Email inválido");
+  } else if (!nomeRegex.test(nome.value)) {
+    alert("Nome inválido");
+  } else if (!numeroRegex.test(numero.value)) {
+    alert("Número inválido");
+  } else {
+    alert("Dados salvos com sucesso");
+  }
+}
+let numero = "44991230106";
+
+if (numeroRegex.test(numero)) {
+  console.log("numero correto");
+} else {
+  console.log("nome incorreto")
+}
